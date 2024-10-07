c4fc8c627483c97a5dd58e902fc7f0202e5e6154
Harshitanay <141206504+Harshitanay@users.noreply.github.com>
2024-10-07 13:43:21 +0530

    Add files via upload

--- node.js
+++ node.js
@@ -1,54 +1,15 @@
-// server.js
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const cors = require('cors');
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// MongoDB connection
-mongoose.connect('mongodb://localhost/simActivation', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// SIM Schema
-const simSchema = new mongoose.Schema({
-    simNumber: String,
-    phoneNumber: String,
-    userName: String,
-    activationStatus: { type: Boolean, default: false },
-});
-
-const SIM = mongoose.model('SIM', simSchema);
-
-// POST: Activate SIM card
-app.post('/api/activate', async (req, res) => {
-    const { simNumber, phoneNumber, userName } = req.body;
-    
-    let sim = await SIM.findOne({ simNumber });
-    if (!sim) {
-        return res.status(404).send('SIM card not found.');
-    }
-    
-    sim.phoneNumber = phoneNumber;
-    sim.userName = userName;
-    sim.activationStatus = true;
-    
-    await sim.save();
-    res.send('SIM activated successfully.');
-});
-
-// GET: Check SIM status
-app.get('/api/status/:simNumber', async (req, res) => {
-    const sim = await SIM.findOne({ simNumber: req.params.simNumber });
-    
-    if (!sim) {
-        return res.status(404).send('SIM card not found.');
-    }
-    
-    res.send({ activated: sim.activationStatus });
-});
-
-// Listen to port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const index_1 = __importDefault(require("./index"));
+const memory_code_points_1 = require("./memory-code-points");
+const code_points_data_1 = __importDefault(require("./code-points-data"));
+const codePoints = (0, memory_code_points_1.createMemoryCodePoints)(code_points_data_1.default);
+function saslprep(input, opts) {
+    return (0, index_1.default)(codePoints, input, opts);
+}
+saslprep.saslprep = saslprep;
+saslprep.default = saslprep;
+module.exports = saslprep;
+//# sourceMappingURL=node.js.map
